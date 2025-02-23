ef616307355efbfd065a9791f78cf1a3ee133131
nagaraj6618 <nagaraj516700@gmail.com>
2025-02-24 04:57:32 +0530

    Updated chat delete function

--- index.js
+++ index.js
@@ -12,7 +12,10 @@ const PORT = process.env.PORT || 8000
 
 app.use(cors());
 app.use(express.json());
-
+app.use(cors({
+   origin:"*",
+   credentials:true
+}))
 //DB connection
 function dbConnection () {
    mongoose.connect(process.env.MONGODB_URL)
